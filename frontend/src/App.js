7de672570e5d5f21e9439619eadbf4237859745f
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-04 20:37:37 +0530

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/HomeM';
 import CategoryPage from './Components/Category/CategoryPage';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -11,9 +12,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} exact />
+          <Route path="/blog" element={<Home />} exact />
+          <Route path="/contactus" element={<Home />} exact />
+          <Route path="/FAQ" element={<Home />} exact />
+          <Route path="/newarrivals" element={<Home />} exact />
+          <Route path="/shop" element={<Home />} exact />
+          <Route path="/aboutus" element={<Home />} exact />
           <Route path="/category" element={<CategoryPage />} />
           <Route path="/category/:type" element={<CategoryPage />} />
         </Routes>
+        <Footer email="kozaleather@help.com"/>
       </Router>
     </>
   )
