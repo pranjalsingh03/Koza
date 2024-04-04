3fa677872c66a34bfb12672930993883cffa381c
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-04 18:20:52 +0530

    updated the products for fetching the data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,52 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
+const PORT = 3001;
 
-app.get("/api",(req, res)=>{
-    res.json({"user":["one","two","three"]})
-})
+const products = [
+    {
+        "id": 1,
+        "name": "brandMe Men's Leather Blazer Genuine Soft Lambskin Coat Jacket BB25",
+        "image": "https://www.62icon.com/client/assets/img/like-icon.svg",
+        "price": 11585.84,
+        "discount": 12
+    },
+    {
+        id: 2,
+        name: "KL Koza Leathers Men’s Genuine Lambskin Leather Jacket KP005",
+        image: "https://www.62icon.com/client/assets/img/like-icon.svg",
+        price: 11252.44,
+        discount: 15
+    },
+    {
+        id: 3,
+        name: "KL Koza Leathers Men’s Genuine Lambskin Leather Jacket KP005",
+        image: "https://www.62icon.com/client/assets/img/like-icon.svg",
+        price: 11252.44,
+        discount: 15
+    },
+    {
+        id: 4,
+        name: "KL Koza Leathers Men’s Genuine Lambskin Leather Jacket KP005",
+        image: "https://www.62icon.com/client/assets/img/like-icon.svg",
+        price: 11252.44,
+        discount: 15
+    },
+    {
+        id: 5,
+        name: "KL Koza Leathers Men’s Genuine Lambskin Leather Jacket KP005",
+        image: "https://www.62icon.com/client/assets/img/like-icon.svg",
+        price: 11252.44,
+        discount: 15
+    },
+];
 
-app.listen(4000, ()=>{
-    console.log("server is running");
+app.use(cors());
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
