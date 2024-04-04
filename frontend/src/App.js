55f608c8be16924b0939172b98e752823be202b3
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-04 20:21:00 +0530

    update: route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/HomeM';
+import CategoryPage from './Components/Category/CategoryPage';
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Home/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/category" element={<CategoryPage />} />
+          <Route path="/category/:type" element={<CategoryPage />} />
+        </Routes>
       </Router>
     </>
   )
