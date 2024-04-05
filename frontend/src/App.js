c7801c4bbdacfe00b817fb5db160da274562ca26
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-05 19:24:47 +0530

    Add Blog component and update route paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/HomeM';
 import CategoryPage from './Components/Category/CategoryPage';
 import Footer from './Components/Footer/Footer';
+import Blog from './Components/Blog/Blog';
+import NewArrivals from './Components/Arrivals/NewArrivals';
+import Login from './Components/Auth/Login';
+import Signup from './Components/Auth/Signup';
 
 function App() {
   return (
@@ -11,11 +15,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="/login" element={<Login />} exact />
+          <Route path="/signup" element={<Signup />} exact />
           <Route path="/" element={<Home />} exact />
-          <Route path="/blog" element={<Home />} exact />
+          <Route path="/blogs" element={<Blog />} exact />
           <Route path="/contactus" element={<Home />} exact />
           <Route path="/FAQ" element={<Home />} exact />
-          <Route path="/newarrivals" element={<Home />} exact />
+          <Route path="/newarrivals" element={<NewArrivals />} exact />
           <Route path="/shop" element={<Home />} exact />
           <Route path="/aboutus" element={<Home />} exact />
           <Route path="/category" element={<CategoryPage />} />
---
+++ frontend/src/Components/Blog/Blog.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/blogs');
+                setBlogs(response.data);
+            } catch (error) {
+                console.error('Error fetching blogs:', error);
+            }
+        };
+
+        fetchBlogs();
+    }, []);
+
+    return (
+        <div className="container mx-auto py-8">
+    <h1 className="text-3xl font-bold mb-4">Blog</h1>
+    {blogs.map(blog => (
+        <div key={blog._id} className="bg-white shadow-md rounded-md p-6 mb-4">
+            <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
+            <p className="text-gray-700">{blog.content}</p>
+            <div className="flex items-center mt-2">
+                <p className="text-gray-600 mr-4">By {blog.author}</p>
+                <p className="text-gray-600">Date: {new Date(blog.date).toLocaleDateString()}</p>
+            </div>
+        </div>
+    ))}
+</div>
+
+    );
+}
+
+export default Blog;
