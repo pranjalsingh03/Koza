e7a7ee4c3f059ae5df50f05e213211e8a06a601f
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-08 22:34:09 +0530

    Update Navbar component with fixed position and new styles

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -23,7 +23,13 @@ const Navbar = () => {
                     navLinks.classList.remove('fixed', 'top-0', 'w-full', 'bg-main', 'z-50', 'shadow-md');
                 }
             }
+            if (scrollPosition > 90) {
+                navLinks.classList.add('fixed', 'top-0', 'w-full', 'bg-main', 'z-50', 'shadow-md');
+            } else {
+                navLinks.classList.remove('fixed', 'top-0', 'w-full', 'bg-main', 'z-50', 'shadow-md');
+            }
         };
+
 
         window.addEventListener('scroll', handleScroll);
 
@@ -34,19 +40,20 @@ const Navbar = () => {
 
     return (
         <nav className="navbar-main p-4 bg-main flex items-center justify-between fixed top-0 w-full z-50 shadow-md" id="navHead">
-            <div className="flex items-center justify-center md:justify-start">
+            <div className="flex items-center">
                 <button className="text-gray-700 mr-4 md:hidden" onClick={toggleMenu}>
                     {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
                 </button>
-                <h1 className="text-gray-700 text-3xl font-bold">kůže</h1>
+                <h1 className="text-gray-700 text-lg font-bold">kůže</h1>
             </div>
-            <ul className={`md:flex md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
-                <li><NavLink to="/" className="text-gray-700" activeClassName="font-bold">Home</NavLink></li>
-                <li><NavLink to="/womens" className="text-gray-700" activeClassName="font-bold">Womens</NavLink></li>
-                <li><NavLink to="/mens" className="text-gray-700" activeClassName="font-bold">Mens</NavLink></li>
-                <li><NavLink to="/newarrivals" className="text-gray-700" activeClassName="font-bold">New Arrivals</NavLink></li>
-                <li><NavLink to="/blogs" className="text-gray-700" activeClassName="font-bold">Blogs</NavLink></li>
-                <li><NavLink to="/FAQ" className="text-gray-700" activeClassName="font-bold">FAQ</NavLink></li>
+            <div className={`md:hidden ${isOpen ? 'fixed inset-0 bg-gray-900 bg-opacity-50 z-50' : 'hidden'}`} onClick={toggleMenu}></div>
+            <ul className={`md:flex md:space-x-4 md:items-center md:pt-4 md:pr-4 md:border-l ${isOpen ? 'fixed top-0 right-0 h-screen w-4/5 bg-zinc-900 p-4 z-50 flex-col justify-center' : 'hidden'}`}>
+                <li><NavLink to="/" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`} activeClassName="font-bold" onClick={toggleMenu}>Home</NavLink></li>
+                <li><NavLink to="/womens" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`} activeClassName="font-bold" onClick={toggleMenu}>Womens</NavLink></li>
+                <li><NavLink to="/mens" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>Mens</NavLink></li>
+                <li><NavLink to="/newarrivals" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>New Arrivals</NavLink></li>
+                <li><NavLink to="/blogs" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>Blogs</NavLink></li>
+                <li><NavLink to="/FAQ" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>FAQ</NavLink></li>
             </ul>
             <div className="flex items-center">
                 <Heart className="h-6 w-6 text-gray-700 mr-4" />
