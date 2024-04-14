e55f3ff4d4a0a8a0bfd2d298f0e5c8b1a8abfbc6
Restyled.io <commits@restyled.io>
2024-04-14 19:58:09 +0000

    Restyled by prettier

--- admin/routes/routes.js
+++ admin/routes/routes.js
@@ -1,76 +1,82 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../models/productModel');
-const multer = require('multer');
-const fs = require('fs');
+const Product = require("../models/productModel");
+const multer = require("multer");
+const fs = require("fs");
 
 var storage = multer.diskStorage({
-  destination : function(req, file, cb) { cb(null, './uploads') },
-  filename : function(req, file, cb) {
-    cb(null, file.filename + '_' + Date.now() + "_" + file.originalname)
-  }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename + "_" + Date.now() + "_" + file.originalname);
+  },
 });
 
-var upload = multer({storage : storage}).single('image');
+var upload = multer({ storage: storage }).single("image");
 
-router.get("/", (req, res) => {res.render('index', {title : 'Home Page'})});
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
 
-router.get('/product', async (req, res) => {
+router.get("/product", async (req, res) => {
   const products = await Product.find();
-  res.render('product', {title : 'Products', products : products})
+  res.render("product", { title: "Products", products: products });
 });
 
-router.get('/addproduct',
-           (req, res) => {res.render('addproduct', {title : "Add Products"})})
+router.get("/addproduct", (req, res) => {
+  res.render("addproduct", { title: "Add Products" });
+});
 
-router.get('/addblog',
-           (req, res) => {res.render('addblog', {title : 'Add Blogs'})})
+router.get("/addblog", (req, res) => {
+  res.render("addblog", { title: "Add Blogs" });
+});
 
-router.post('/addproduct', upload, async (req, res) => {
+router.post("/addproduct", upload, async (req, res) => {
   try {
     const product = new Product({
-      name : req.body.name,
-      image : req.file.filename,
-      price : req.body.price,
-      discount : req.body.discount,
-      category : req.body.category
+      name: req.body.name,
+      image: req.file.filename,
+      price: req.body.price,
+      discount: req.body.discount,
+      category: req.body.category,
     });
     await product.save();
     req.session.message = {
-      type : 'success',
-      message : 'Product Added Successfully'
+      type: "success",
+      message: "Product Added Successfully",
     };
-    res.redirect('/product');
+    res.redirect("/product");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // edit the product
-router.get('/edit/:id', async (req, res) => {
+router.get("/edit/:id", async (req, res) => {
   let id = req.params.id;
   try {
     const product = await Product.findById(id).exec();
     if (!product) {
-      return res.redirect('/product');
+      return res.redirect("/product");
     }
-    res.render('editproduct', {title : 'Edit Product', product : product});
+    res.render("editproduct", { title: "Edit Product", product: product });
   } catch (error) {
     console.error(error);
-    res.redirect('/product');
+    res.redirect("/product");
   }
 });
 
 // Update the product
-router.post('/update/:id', upload, async (req, res) => {
+router.post("/update/:id", upload, async (req, res) => {
   try {
     let id = req.params.id;
-    let new_image = '';
+    let new_image = "";
     if (req.file) {
       new_image = req.file.filename;
       try {
-        fs.unlinkSync('./uploads/' + req.body.old_image);
+        fs.unlinkSync("./uploads/" + req.body.old_image);
       } catch (err) {
         console.log(err);
       }
@@ -79,21 +85,21 @@ router.post('/update/:id', upload, async (req, res) => {
     }
 
     await Product.findByIdAndUpdate(id, {
-      name : req.body.name,
-      image : new_image,
-      price : req.body.price,
-      discount : req.body.discount,
-      category : req.body.category
+      name: req.body.name,
+      image: new_image,
+      price: req.body.price,
+      discount: req.body.discount,
+      category: req.body.category,
     });
 
     req.session.message = {
-      type : 'success',
-      message : 'Product Updated Successfully'
+      type: "success",
+      message: "Product Updated Successfully",
     };
-    res.redirect('/product');
+    res.redirect("/product");
   } catch (error) {
     console.error(error);
-    res.redirect('/product');
+    res.redirect("/product");
   }
 });
 
@@ -105,20 +111,20 @@ router.get("/delete/:id", async (req, res) => {
 
     if (product.image) {
       try {
-        fs.unlinkSync('./uploads/' + product.image);
+        fs.unlinkSync("./uploads/" + product.image);
       } catch (err) {
         console.log(err);
       }
     }
 
     req.session.message = {
-      type : 'success',
-      message : 'Product Deleted Successfully'
+      type: "success",
+      message: "Product Deleted Successfully",
     };
-    res.redirect('/product');
+    res.redirect("/product");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
