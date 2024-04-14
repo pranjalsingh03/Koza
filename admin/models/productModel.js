e72ef8cd6f71de0b5ed73f77f64b720a2bdf5b7c
Restyled.io <commits@restyled.io>
2024-04-14 19:58:08 +0000

    Restyled by clang-format

--- admin/models/productModel.js
+++ admin/models/productModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    price: Number,
-    discount: Number,
-    category: String
+  name : String,
+  image : String,
+  price : Number,
+  discount : Number,
+  category : String
 });
 
 module.exports = mongoose.model('Product', productSchema);
--- admin/routes/routes.js
+++ admin/routes/routes.js
@@ -5,131 +5,121 @@ const multer = require('multer');
 const fs = require('fs');
 
 var storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'./uploads')
-    },
-    filename: function(req,file,cb){
-        cb(null,file.filename+'_'+Date.now()+"_"+file.originalname)
-    }
+  destination : function(req, file, cb) { cb(null, './uploads') },
+  filename : function(req, file, cb) {
+    cb(null, file.filename + '_' + Date.now() + "_" + file.originalname)
+  }
 });
 
-var upload = multer({
-    storage:storage
-}).single('image');
+var upload = multer({storage : storage}).single('image');
 
-router.get("/",(req,res)=>{
-    res.render('index',{title:'Home Page'})
+router.get("/", (req, res) => {res.render('index', {title : 'Home Page'})});
+
+router.get('/product', async (req, res) => {
+  const products = await Product.find();
+  res.render('product', {title : 'Products', products : products})
 });
 
-router.get('/product',async(req,res)=>{
-    const products = await Product.find();
-    res.render('product',{title:'Products',products:products})
-});
+router.get('/addproduct',
+           (req, res) => {res.render('addproduct', {title : "Add Products"})})
 
-router.get('/addproduct',(req,res)=>{
-    res.render('addproduct',{title:"Add Products"})
-})
-
-router.get('/addblog',(req,res)=>{
-    res.render('addblog',{title:'Add Blogs'})
-})
+router.get('/addblog',
+           (req, res) => {res.render('addblog', {title : 'Add Blogs'})})
 
 router.post('/addproduct', upload, async (req, res) => {
-    try {
-        const product = new Product({
-            name: req.body.name,
-            image: req.file.filename,
-            price: req.body.price,
-            discount: req.body.discount,
-            category: req.body.category
-        });
-        await product.save();
-        req.session.message = {
-            type: 'success',
-            message: 'Product Added Successfully'
-        };
-        res.redirect('/product');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    const product = new Product({
+      name : req.body.name,
+      image : req.file.filename,
+      price : req.body.price,
+      discount : req.body.discount,
+      category : req.body.category
+    });
+    await product.save();
+    req.session.message = {
+      type : 'success',
+      message : 'Product Added Successfully'
+    };
+    res.redirect('/product');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-//edit the product
+// edit the product
 router.get('/edit/:id', async (req, res) => {
-    let id = req.params.id;
-    try {
-        const product = await Product.findById(id).exec();
-        if (!product) {
-            return res.redirect('/product');
-        }
-        res.render('editproduct', { title: 'Edit Product', product: product });
-    } catch (error) {
-        console.error(error);
-        res.redirect('/product');
+  let id = req.params.id;
+  try {
+    const product = await Product.findById(id).exec();
+    if (!product) {
+      return res.redirect('/product');
     }
+    res.render('editproduct', {title : 'Edit Product', product : product});
+  } catch (error) {
+    console.error(error);
+    res.redirect('/product');
+  }
 });
 
-//Update the product
+// Update the product
 router.post('/update/:id', upload, async (req, res) => {
-    try {
-        let id = req.params.id;
-        let new_image = '';
-        if (req.file) {
-            new_image = req.file.filename;
-            try {
-                fs.unlinkSync('./uploads/' + req.body.old_image);
-            } catch (err) {
-                console.log(err);
-            }
-        } else {
-            new_image = req.body.old_image;
-        }
-
-        await Product.findByIdAndUpdate(id, {
-            name: req.body.name,
-            image: new_image,
-            price: req.body.price,
-            discount: req.body.discount,
-            category: req.body.category
-        });
-
-        req.session.message = {
-            type: 'success',
-            message: 'Product Updated Successfully'
-        };
-        res.redirect('/product');
-    } catch (error) {
-        console.error(error);
-        res.redirect('/product');
+  try {
+    let id = req.params.id;
+    let new_image = '';
+    if (req.file) {
+      new_image = req.file.filename;
+      try {
+        fs.unlinkSync('./uploads/' + req.body.old_image);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      new_image = req.body.old_image;
     }
+
+    await Product.findByIdAndUpdate(id, {
+      name : req.body.name,
+      image : new_image,
+      price : req.body.price,
+      discount : req.body.discount,
+      category : req.body.category
+    });
+
+    req.session.message = {
+      type : 'success',
+      message : 'Product Updated Successfully'
+    };
+    res.redirect('/product');
+  } catch (error) {
+    console.error(error);
+    res.redirect('/product');
+  }
 });
 
-
-//delete a particular record from database using delete method
+// delete a particular record from database using delete method
 router.get("/delete/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const product = await Product.findByIdAndDelete(id);
+  try {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
 
-        if (product.image) {
-            try {
-                fs.unlinkSync('./uploads/' + product.image);
-            } catch (err) {
-                console.log(err);
-            }
-        }
-
-        req.session.message = {
-            type: 'success',
-            message: 'Product Deleted Successfully'
-        };
-        res.redirect('/product');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+    if (product.image) {
+      try {
+        fs.unlinkSync('./uploads/' + product.image);
+      } catch (err) {
+        console.log(err);
+      }
     }
-});
 
+    req.session.message = {
+      type : 'success',
+      message : 'Product Deleted Successfully'
+    };
+    res.redirect('/product');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 module.exports = router;
