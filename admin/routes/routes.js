258813933d0e5a18e38c4c561ae1f579c57ba897
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-15 01:26:03 +0530

    Refactor file upload and form validation in addproduct.ejs and editproduct.ejs views

--- admin/routes/routes.js
+++ admin/routes/routes.js
@@ -2,17 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/productModel');
 const multer = require('multer');
+const fs = require('fs');
 
 var storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'./images')
+        cb(null,'./uploads')
     },
     filename: function(req,file,cb){
-        cb(null,file.originalname)
+        cb(null,file.filename+'_'+Date.now()+"_"+file.originalname)
     }
 });
 
-var upload = multer({storage:storage}).single('image');
+var upload = multer({
+    storage:storage
+}).single('image');
 
 router.get("/",(req,res)=>{
     res.render('index',{title:'Home Page'})
@@ -31,60 +34,102 @@ router.get('/addblog',(req,res)=>{
     res.render('addblog',{title:'Add Blogs'})
 })
 
-router.post('/addproduct',upload ,async(req,res)=>{
-    const product = new Product({
-        name:req.body.name,
-        image:req.file.originalname,
-        price:req.body.price,
-        discount:req.body.discount
-    })
-    try{
-        await product.save();
-        res.redirect('/product');
-    }catch(err){
-        console.log(err);
-    }
-})
-
-//update the data in database using put method 
-router.put("/edit/:id", upload, async (req, res) => {
-    let product;
+router.post('/addproduct', upload, async (req, res) => {
     try {
-        product = await Product.findById(req.params.id);
-        // Checking for old and new images
-        if (!req.file) {
-            product.name = req.body.name;
-            product.price = req.body.price;
-            product.discount = req.body.discount;
-            await product.save();
-        } else {
-            product.image = req.file.originalname;
-            product.name = req.body.name;
-            delete req.body.image;
-            Object.assign(product, req.body);
-            await product.save();
-        }
+        const product = new Product({
+            name: req.body.name,
+            image: req.file.filename,
+            price: req.body.price,
+            discount: req.body.discount,
+            category: req.body.category
+        });
+        await product.save();
+        req.session.message = {
+            type: 'success',
+            message: 'Product Added Successfully'
+        };
         res.redirect('/product');
     } catch (error) {
-        console.log(error);
-        res.status(500).send("Internal Server Error");
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
-//delete a particular record from database using delete method
-router.get("/delete/:id", async(req,res)=>{
+//edit the product
+router.get('/edit/:id', async (req, res) => {
     let id = req.params.id;
-    try{
-        await Product.findByIdAndRemove(id ,(err,product)=>{
-            if(err){
+    try {
+        const product = await Product.findById(id).exec();
+        if (!product) {
+            return res.redirect('/product');
+        }
+        res.render('editproduct', { title: 'Edit Product', product: product });
+    } catch (error) {
+        console.error(error);
+        res.redirect('/product');
+    }
+});
+
+//Update the product
+router.post('/update/:id', upload, async (req, res) => {
+    try {
+        let id = req.params.id;
+        let new_image = '';
+        if (req.file) {
+            new_image = req.file.filename;
+            try {
+                fs.unlinkSync('./uploads/' + req.body.old_image);
+            } catch (err) {
                 console.log(err);
             }
+        } else {
+            new_image = req.body.old_image;
+        }
+
+        await Product.findByIdAndUpdate(id, {
+            name: req.body.name,
+            image: new_image,
+            price: req.body.price,
+            discount: req.body.discount,
+            category: req.body.category
         });
 
+        req.session.message = {
+            type: 'success',
+            message: 'Product Updated Successfully'
+        };
         res.redirect('/product');
-    }catch(e){
-        console.log(e);
+    } catch (error) {
+        console.error(error);
+        res.redirect('/product');
     }
-})
+});
+
+
+//delete a particular record from database using delete method
+router.get("/delete/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (product.image) {
+            try {
+                fs.unlinkSync('./uploads/' + product.image);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        req.session.message = {
+            type: 'success',
+            message: 'Product Deleted Successfully'
+        };
+        res.redirect('/product');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 module.exports = router;
