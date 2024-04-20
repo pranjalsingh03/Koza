32b41888294031f5d9a15ce6c229f139be0ec447
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-20 23:37:25 +0530

    feat: increament addtocart

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect ,useContext} from 'react';
 import { NavLink ,Link } from 'react-router-dom';
 import { Menu, Heart, ShoppingCart, X } from 'lucide-react';
+import { ShopContext } from '../../Context/ShopContextProvider';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const {getTotalCartItem}= useContext(ShopContext);
+
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -14,8 +17,6 @@ const Navbar = () => {
             const scrollPosition = window.scrollY;
             const screenWidth = window.innerWidth;
             const navLinks = document.getElementById('navHead');
-
-            // Apply scroll effect only on mobile devices (screen width less than or equal to 768 pixels)
             if (screenWidth <= 768) {
                 if (scrollPosition > 90) {
                     navLinks.classList.add('fixed', 'top-0', 'w-full', 'bg-main', 'z-50', 'shadow-md');
@@ -48,18 +49,19 @@ const Navbar = () => {
             </div>
             <div className={`md:hidden ${isOpen ? 'fixed inset-0 bg-gray-900 bg-opacity-50 z-50' : 'hidden'}`} onClick={toggleMenu}></div>
             <ul className={`md:flex md:space-x-4 md:items-center md:pt-4 md:pr-4 md:border-l ${isOpen ? 'fixed top-0 right-0 h-screen w-4/5 bg-zinc-900 p-4 z-50 flex-col justify-center' : 'hidden'}`}>
-                <li><NavLink to="/" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`} activeClassName="font-bold" onClick={toggleMenu}>Home</NavLink></li>
-                <li><NavLink to="/womens" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`} activeClassName="font-bold" onClick={toggleMenu}>Womens</NavLink></li>
-                <li><NavLink to="/mens" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>Mens</NavLink></li>
-                <li><NavLink to="/newarrivals" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>New Arrivals</NavLink></li>
-                <li><NavLink to="/blogs" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>Blogs</NavLink></li>
-                <li><NavLink to="/FAQ" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" onClick={toggleMenu}>FAQ</NavLink></li>
+                <li><NavLink to="/" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`} activeClassName="font-bold" >Home</NavLink></li>
+                <li><NavLink to="/womens" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`} activeClassName="font-bold" >Womens</NavLink></li>
+                <li><NavLink to="/mens" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" >Mens</NavLink></li>
+                <li><NavLink to="/newarrivals" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold">New Arrivals</NavLink></li>
+                <li><NavLink to="/blogs" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold">Blogs</NavLink></li>
+                <li><NavLink to="/FAQ" className={`text-gray-700 ${isOpen ? 'text-white': 'text-gray-700'}`}activeClassName="font-bold" >FAQ</NavLink></li>
             </ul>
             <div className="flex items-center">
                 <Heart className="h-6 w-6 text-gray-700 mr-4" />
                 <Link to="/cart">
                 <ShoppingCart className="h-6 w-6 text-gray-700" />
                 </Link>
+                <div className="nav-cart-count">{getTotalCartItem()}</div>
             </div>
         </nav>
     );
