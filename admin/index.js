d274c8e21af4206bc379267cc37726b7a2b01462
Restyled.io <commits@restyled.io>
2024-04-22 14:41:43 +0000

    Restyled by prettier

--- admin/index.js
+++ admin/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 const MONGODB_ADMIN = process.env.MONGODB_CONNECT_URL;
 
 // Middleware
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-    session({secret : 'mysecret', saveUninitialized : false, resave : false}));
-app.use(express.static('uploads'));
-app.set('view engine', 'ejs');
+  session({ secret: "mysecret", saveUninitialized: false, resave: false }),
+);
+app.use(express.static("uploads"));
+app.set("view engine", "ejs");
 app.set("layouts", "./layouts/main");
 
 app.use((req, res, next) => {
@@ -26,9 +27,9 @@ app.use((req, res, next) => {
 
 // Database connection
 mongoose
-    .connect(MONGODB_ADMIN, {useNewUrlParser : true, useUnifiedTopology : true})
-    .then(() => console.log('Connected to Database'))
-    .catch(err => console.error('Database connection error:', err));
+  .connect(MONGODB_ADMIN, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.error("Database connection error:", err));
 
 // Routes
 app.use("/", require("./routes/admin"));
@@ -36,7 +37,9 @@ app.use("/", require("./routes/admin"));
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
-app.listen(PORT, () => { console.log(`Server is running on port:${PORT}`); });
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
--- admin/routes/admin.js
+++ admin/routes/admin.js
@@ -10,63 +10,66 @@ const adminLayout = "../views/layouts/admin";
 const jwtSecret = process.env.JWT_Key;
 
 var storage = multer.diskStorage({
-  destination : function(req, file, cb) { cb(null, './uploads') },
-  filename : function(req, file, cb) {
-    cb(null, file.filename + '_' + Date.now() + "_" + file.originalname)
-  }
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename + "_" + Date.now() + "_" + file.originalname);
+  },
 });
 
-var upload = multer({storage : storage}).single('image');
+var upload = multer({ storage: storage }).single("image");
 
 const authMiddleware = (req, res, next) => {
   const token = req.cookies.token;
 
   if (!token) {
-    return res.status(401).json({message : "Unauthorized"});
+    return res.status(401).json({ message: "Unauthorized" });
   }
   try {
     const decoded = jwt.verify(token, jwtSecret);
     req.userId = decoded.userId;
     next();
   } catch (error) {
-    res.status(401).json({message : "Unauthorized"});
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
 
 router.get("/admin", async (req, res) => {
   try {
     const locals = {
-      title : "Admin",
-      description : "NodeJS in MongoDB",
+      title: "Admin",
+      description: "NodeJS in MongoDB",
     };
 
-    res.render("admin/index", {locals, layout : adminLayout});
+    res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
   }
 });
 
-router.get("/",
-           (req, res) => {res.render("admin/index", {title : "Admin Login"})})
+router.get("/", (req, res) => {
+  res.render("admin/index", { title: "Admin Login" });
+});
 
 router.post("/admin", async (req, res) => {
   try {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    const user = await User.findOne({username});
+    const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(401).json({message : "Invalid credentials"});
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(401).json({message : "Invalid credentials"});
+      return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({userId : user._id}, jwtSecret);
-    res.cookie("token", token, {httpOnly : true});
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
 
     res.redirect("/admin/product");
   } catch (error) {
@@ -76,61 +79,61 @@ router.post("/admin", async (req, res) => {
 
 router.post("/register", async (req, res) => {
   try {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     try {
-      const user = await User.create({username, password : hashedPassword});
-      res.status(201).json({message : "User created!", user});
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User created!", user });
     } catch (error) {
       if (error.code === 11000) {
-        res.status(409).json({message : "User already in use"});
+        res.status(409).json({ message: "User already in use" });
       }
-      res.status(500).json({message : "Internal server error"});
+      res.status(500).json({ message: "Internal server error" });
     }
   } catch (error) {
     console.log(error);
   }
-})
+});
 
-router.get('/admin/product', authMiddleware, async (req, res) => {
+router.get("/admin/product", authMiddleware, async (req, res) => {
   const products = await Product.find();
-  res.render('admin/product', {title : 'Products', products : products})
+  res.render("admin/product", { title: "Products", products: products });
 });
 
 router.get("/admin/addproduct", authMiddleware, async (req, res) => {
   const products = await Product.find();
-  res.render("admin/addproduct", {title : "Add Product", products : products});
+  res.render("admin/addproduct", { title: "Add Product", products: products });
 });
 
-router.post('/addproduct', authMiddleware, upload, async (req, res) => {
+router.post("/addproduct", authMiddleware, upload, async (req, res) => {
   try {
     const product = new Product({
-      name : req.body.name,
-      image : req.file.filename,
-      price : req.body.price,
-      discount : req.body.discount,
-      category : req.body.category
+      name: req.body.name,
+      image: req.file.filename,
+      price: req.body.price,
+      discount: req.body.discount,
+      category: req.body.category,
     });
     await product.save();
     req.session.message = {
-      type : 'success',
-      message : 'Product Added Successfully'
+      type: "success",
+      message: "Product Added Successfully",
     };
-    res.redirect('/admin/product');
+    res.redirect("/admin/product");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
 
-router.post('/admin/update/:id', authMiddleware, upload, async (req, res) => {
+router.post("/admin/update/:id", authMiddleware, upload, async (req, res) => {
   try {
     let id = req.params.id;
-    let new_image = '';
+    let new_image = "";
     if (req.file) {
       new_image = req.file.filename;
       try {
-        fs.unlinkSync('./uploads/' + req.body.old_image);
+        fs.unlinkSync("./uploads/" + req.body.old_image);
       } catch (err) {
         console.log(err);
       }
@@ -139,21 +142,21 @@ router.post('/admin/update/:id', authMiddleware, upload, async (req, res) => {
     }
 
     await Product.findByIdAndUpdate(id, {
-      name : req.body.name,
-      image : new_image,
-      price : req.body.price,
-      discount : req.body.discount,
-      category : req.body.category
+      name: req.body.name,
+      image: new_image,
+      price: req.body.price,
+      discount: req.body.discount,
+      category: req.body.category,
     });
 
     req.session.message = {
-      type : 'success',
-      message : 'Product Updated Successfully'
+      type: "success",
+      message: "Product Updated Successfully",
     };
-    res.redirect('/admin/product');
+    res.redirect("/admin/product");
   } catch (error) {
     console.error(error);
-    res.redirect('/admin/product');
+    res.redirect("/admin/product");
   }
 });
 
@@ -164,23 +167,25 @@ router.get("/admin/delete/:id", async (req, res) => {
 
     if (product.image) {
       try {
-        fs.unlinkSync('./uploads/' + product.image);
+        fs.unlinkSync("./uploads/" + product.image);
       } catch (err) {
         console.log(err);
       }
     }
 
     req.session.message = {
-      type : 'success',
-      message : 'Product Deleted Successfully'
+      type: "success",
+      message: "Product Deleted Successfully",
     };
-    res.redirect('/admin/product');
+    res.redirect("/admin/product");
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 });
-router.get("/admin/highLightTitle.png", (req, res) => {res.send("you get it!")})
+router.get("/admin/highLightTitle.png", (req, res) => {
+  res.send("you get it!");
+});
 
 router.get("/admin/edit/:id", authMiddleware, async (req, res) => {
   try {
@@ -189,8 +194,10 @@ router.get("/admin/edit/:id", authMiddleware, async (req, res) => {
     if (!product) {
       return res.redirect("/admin/product");
     }
-    res.render("admin/editproduct",
-               {title : "Edit Product", product : product});
+    res.render("admin/editproduct", {
+      title: "Edit Product",
+      product: product,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
@@ -215,8 +222,8 @@ router.put("/admin/edit/:id", authMiddleware, async (req, res) => {
     await product.save();
 
     req.session.message = {
-      type : "success",
-      message : "Product Updated Successfully",
+      type: "success",
+      message: "Product Updated Successfully",
     };
     res.redirect("/admin/product");
   } catch (error) {
@@ -233,8 +240,8 @@ router.delete("/admin/removeproduct/:id", authMiddleware, async (req, res) => {
       return res.status(404).send("Product not found");
     }
     req.session.message = {
-      type : "success",
-      message : "Product Deleted Successfully",
+      type: "success",
+      message: "Product Deleted Successfully",
     };
     res.redirect("/admin/product");
   } catch (error) {
