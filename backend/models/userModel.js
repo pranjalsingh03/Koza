cc8014596d97946fd8abe8345147ed4737284f65
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-23 23:45:51 +0530

    update: models

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,21 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Product = require("./productModel")
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    cart: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product'
-    }]
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    cart: [{ type: Schema.Types.ObjectId, ref: Product }],
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
