2088bc833cd6f58438c22f609efa7a9c5d399f36
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-26 20:23:38 +0530

    Refactor Cart component to fetch cart items from API and integrate Razorpay checkout

--- frontend/src/Components/AddtoCart/Cart.js
+++ frontend/src/Components/AddtoCart/Cart.js
@@ -1,28 +1,77 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { ShopContext } from '../../Context/ShopContextProvider';
 import Newsletter from '../Newsletter/Newsletter';
+import axios from 'axios';
+import NewArrivals from '../Arrivals/NewArrivals';
+import ReviewList from '../../Review/ReviewList';
+import ReviewSection from '../../Review/ReviewSection';
 
 function Cart() {
-    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+    const {addToCart, removeFromCart } = useContext(ShopContext);
     const [totalPrice, setTotalPrice] = useState(0);
-
+    const [cartItems, setCartItems] = useState([]);
+    
     useEffect(() => {
-        const total = Object.values(cartItems).reduce((acc, item) => acc + item.price * item.quantity, 0);
-        setTotalPrice(total);
-    }, [cartItems]);
+        const getCartItems = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/carts');
+                if (Array.isArray(response.data.cart)) {
+                    setCartItems(response.data.cart);
+                    const total = response.data.cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+                    setTotalPrice(total);
+                } else {
+                    console.log('Invalid data structure received for cart items:', response.data);
+                }
+            } catch (error) {
+                console.log(`Error: ${error}`);
+            }
+        };
+        getCartItems();
+    }, []);
+    
+
+    const handleCheckout = async(totalPrice) => {
+        const { data: {key}} = await axios.get('http://localhost:3001/meowmeow');
+        const { data: {order} } = await axios.post('http://localhost:3001/checkout', {
+            amount: totalPrice
+        });
+        const options = {
+            key,
+            amount: order.amount,
+            currency: "INR",
+            name: "Kuze Leather",
+            description: "Leather Store",
+            image: "https://avatars.githubusercontent.com/u/112399218?v=4",
+            order_id: order.id,
+            callback_url:"http://localhost:3001/isAuthenticated",
+            prefill: {
+                "name": "Pranjal Singh",
+                "email": "pranjalsingh9304@gmail.com",
+                "contact": "9369154040"
+            },
+            notes: {
+                "address": "Lovely Professional University"
+            },
+            theme: {
+                "color": "#3399cc"
+            }
+        };
+        const razor = new window.Razorpay(options);
+        razor.open();
+    };
 
     return (
         <>
             <div className="container mx-auto p-4 pt-28">
                 <h1 className="text-2xl font-bold mb-4">Your Cart</h1>
                 <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-                    {Object.values(cartItems).map((product) => (
+                    {cartItems.map((product) => (
                         <div key={product._id} className="flex justify-between items-center mb-4">
                             <div className="flex items-center">
-                                <img src={product.imageUrl} alt={product.name} className="w-16 h-16 rounded-full mr-4" />
+                                <img src={product.image} alt={product.name} className="w-16 h-16 rounded-full mr-4" />
                                 <div>
                                     <h2 className="text-lg font-bold">{product.name}</h2>
-                                    <p className="text-gray-600">Price: ${product.price}</p>
+                                    <p className="text-gray-600">Price: ₹{product.price}</p>
                                 </div>
                             </div>
                             <div className="flex items-center">
@@ -33,11 +82,14 @@ function Cart() {
                         </div>
                     ))}
                     <div className="mt-4 flex justify-between">
-                        <button className="bg-gray-800 text-white px-4 py-2 rounded-md">Checkout</button>
-                        <p className="text-lg font-bold">Total: ${totalPrice}</p>
+                        <button className="bg-gray-800 text-white px-4 py-2 rounded-md" onClick={handleCheckout}>Checkout</button>
+                        <p className="text-lg font-bold">Total: ₹{totalPrice}</p>
                     </div>
                 </div>
             </div>
+            <ReviewList/>
+            <ReviewSection/>
+            <NewArrivals/>
             <Newsletter />
         </>
     );
